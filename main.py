ac1ecb36db24e52b3e6dc11d61fbc95c259a2943
Justin Paice <justin.paice@gmail.com>
2022-01-24 22:47:26 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import dataclasses
+import json
+class loader:
+    def loaddata(filename):
+        with open(filename, encoding="Latin-1") as json_file:
+            d = json.load(json_file)
+            print("Type:", type(d))
+            return d
+
+data = loader.loaddata('item_superlist.json')
+
+
+for key, value in data.items():
+    if value[0]["Tags"]["Weapon"] == True: 
+        print(key)
+    
---
+++ windowex1.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk()
+greeting = tk.Label(text="Python rocks!")
+greeting.pack()
+btn = tk.Button(text="Heckin")
+btn.pack()
+window.mainloop()

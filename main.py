9390b6ebade97bf40fc46fef1b06e110da914c8e
Montahc <Montahcremire@gmail.com>
2022-01-29 14:11:27 -0500

    tagfiltering2 working version

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import tkinter as tk
 data = loader.loaddata('item_superlist.json')
 
 
-window = tk.Tk(baseName="Report Window")
+window = tk.Tk()
 top_frame = tag_filter_pane(window, data)
-top_frame.grid(row=0, column=0)
+top_frame.frame.grid(row=0, column=0)
 #bot_frame = Frame(window)
 #bot_frame.grid(row=1, column=0)
 
--- tagfiltering2.py
+++ tagfiltering2.py
@@ -1,42 +1,43 @@
 
-from tkinter import *
+import tkinter as tk
 import random
 from record import RecordDisplay as rd
 from style import colors
 
-class tag_filter_pane:
+class tag_filter_pane(tk.Frame):
     def __init__(self, parent, data):
         self.data = data
-        self.frame = Frame(parent)
+        #super(tk.Frame, self).__init__()
+        self.frame = tk.Frame(parent)
         self.tags = []
         self.activetags = {}
         self.labels = []
         self.taglabels = []
-        self.tagprep(self.data, self.tags, self.activetags)
-        self.labelsetup(self.tags, self.activetags, self.frame, self.taglabels)
+        tag_filter_pane.tagprep(self)
+        tag_filter_pane.labelsetup(self)
 
 
-    def tagprep(self, data, tags, activetags):
-        for key, value in data.items():
+    def tagprep(self):
+        for key, value in self.data.items():
             for k, v in value[0]["Tags"].items():
-                if k not in tags:
-                    tags.append(k)
+                if k not in self.tags:
+                    self.tags.append(k)
 
-        for t in tags:
-            activetags[t] = False   
+        for t in self.tags:
+            self.activetags[t] = False   
 
-    def labelsetup(self, tags, activetags, taglabels, frame):
+    def labelsetup(self):
         def change_color(event):
             if event.widget.cget("bg") == colors["ashley_green"]:
                 event.widget.config(bg= colors["no_red"])
-                activetags[event.widget.cget("text")] = False
+                self.activetags[event.widget.cget("text")] = False
             else:
                 event.widget.config(bg=colors["ashley_green"])
-                activetags[event.widget.cget("text")] = True
+                self.activetags[event.widget.cget("text")] = True
             
         x, y = 0, 0
-        for t in tags:
-            l = Label(frame, text=str(t), bg=colors["no_red"], fg="black")
+        for t in self.tags:
+            l = tk.Label(self, text=str(t), bg=colors["no_red"], fg="black")
             l.bind("<Button-1>", lambda event:change_color(event),)
             l.grid(row=y, column=x, sticky="ew")
             if x < 4:
@@ -44,25 +45,25 @@ class tag_filter_pane:
             else:
                 y+=1
                 x=0
-            taglabels.append(l)
-
-            all_on = Label(frame, text="all")
-            all_on.bind("<Button-1>", lambda event:all_true(event))
-            all_off = Label(frame, text="none")
-            all_off.bind("<Button-1>", lambda event:all_false(event))
-            all_on.grid(row=y, column=x)
-            all_off.grid(row=y, column=x+1)
-
-
+            self.taglabels.append(l)
         def all_true(event): 
-            for t in taglabels:
+            for t in self.taglabels:
                 t.config(bg=colors["ashley_green"])
-                activetags[t.cget("text")] = True
+                self.activetags[t.cget("text")] = True
 
         def all_false(event): 
-            for t in taglabels:
+            for t in self.taglabels:
                 t.config(bg=colors["no_red"])
-                activetags[t.cget("text")] = False
+                self.activetags[t.cget("text")] = False
+        all_on = tk.Label(self, text="all")
+        all_on.bind("<Button-1>", lambda event:all_true(event))
+        all_off = tk.Label(self, text="none")
+        all_off.bind("<Button-1>", lambda event:all_false(event))
+        all_on.grid(row=y, column=x)
+        all_off.grid(row=y, column=x+1)
+
+
+
 
 
 
